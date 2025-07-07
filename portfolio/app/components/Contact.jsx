b059cebc123cfaec879658d787cd9339c80c6c2b
abirthapa1 <abirthapa1@gmail.com>
2025-07-07 19:58:38 -0400

    updated access key

--- portfolio/app/components/Contact.jsx
+++ portfolio/app/components/Contact.jsx
@@ -11,7 +11,7 @@ const Contact = () => {
     setResult("Sending....");
     const formData = new FormData(event.target);
 
-    formData.append("access_key", process.env.EMAIL_SENT);
+    formData.append("access_key", process.env.REACT_APP_WEB_FORM_KEY);
 
     const response = await fetch("https://api.web3forms.com/submit", {
       method: "POST",
